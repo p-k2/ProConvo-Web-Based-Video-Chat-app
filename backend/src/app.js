2c51b783f10ab1e48bca60fd7b69b655f4ed48b8
p-k2 <kaurpalak193@gmail.com>
2026-09-07 22:49:08 +0530

    Added routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,6 +7,7 @@ import cors from "cors" ;
 
 import {connectToSocket} from "./controller/socketMAnager.js"
 
+import userRoutes from "./routes/users.routes.js"
 const app = express()
 const server = createServer(app) ;
 const io = connectToSocket(server) ;
@@ -15,6 +16,8 @@ app.set("port" , (process.env.PORT || 8000))
 app.use(cors()) ;
 app.use(express.json({limit: "40kb" })) ;
 app.use(express.urlencoded({ limit: "40kb" , extended: true})) ;
+
+app.use("/api/v1/users" , userRoutes) ;
 
 const start = async()=>{
 
--- backend/src/model/user.model.js
+++ backend/src/model/user.model.js
@@ -1,4 +1,5 @@
-import {Schema} from "mongoose"
+import mongoose ,{Schema} from "mongoose"
+
 
 const userSchema = new Schema({
 
@@ -19,6 +20,5 @@ const userSchema = new Schema({
     token: { type: String}
 })
 
-const User = mongoose.model("user" , userSchema) ;
-
-export {User} ;
+const User = mongoose.model('User', userSchema);
+export default User;
